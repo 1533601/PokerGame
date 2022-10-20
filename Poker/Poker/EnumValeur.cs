a34b3e043787109d742a57e90bffb61f8e66f06c
Famous Bald Guy <112105406+1533601@users.noreply.github.com>
2022-10-20 12:25:27 -0400

    Base
    Base

---
+++ Poker/Poker/EnumValeur.cs
@@ -0,0 +1,26 @@
+﻿namespace PokerGame
+{
+    enum Valeur
+    {
+        Deux =2,
+        Trois,
+        Quatre,
+        Cinq,
+        Six,
+        Sept,
+        Huit,
+        Neuf,
+        Dix,
+        Valet,
+        reine,
+        Roi,
+        As,
+    }
+    enum Couleur
+    {
+        Coueur,
+        Pique,
+        Carreau,
+        Treffle
+    }
+}
---
+++ Poker/Poker/Joueur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerGame
+{
+    internal class Joueur
+    {
+        string nom;
+        string pseudo;
+        int argent;
+        bool actif;
+        MainJoueur maMain;
+    }
+}
