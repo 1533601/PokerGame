d047d7b66bef9c5ba3a738723ceb76546ad319e8
samuel <1830803@etu.cchic.ca>
2022-11-02 19:21:50 -0400

    création de l'affichage

--- Poker/Poker/Partie.cs
+++ Poker/Poker/Partie.cs
@@ -30,7 +30,204 @@ namespace PokerGame
         }
         public void AfficherJeu()
         {
-            throw new NotImplementedException();
+            //table de jeux
+            Console.SetCursorPosition(15, 13);
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.SetCursorPosition(15, 14 + i);
+                Console.WriteLine("|                                                                                  |");
+            }
+            Console.SetCursorPosition(15, 25);
+            Console.WriteLine("------------------------------------------------------------------------------------");
+
+
+            //carte du millieux
+            Console.SetCursorPosition(20, 16);
+            Console.WriteLine("----------      ----------      ----------      ----------      ----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(20, 17 + i);
+                Console.WriteLine("|        |      |        |      |        |      |        |      |        |");
+            }
+            Console.SetCursorPosition(20, 22);
+            Console.WriteLine("----------      ----------      ----------      ----------      ----------");
+
+
+
+            //nom joueur 1
+
+            Console.SetCursorPosition(20, 1);
+            Console.WriteLine("montent jour11");
+
+            //montent joueur 1
+
+            Console.SetCursorPosition(36, 1);
+            Console.WriteLine("nom joueur");
+
+            //premier carte joueur 1
+
+            Console.SetCursorPosition(20, 3);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(20, 4 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(20, 9);
+            Console.WriteLine("----------");
+
+            //deuxiem carete joueur 1
+
+            Console.SetCursorPosition(36, 3);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(36, 4 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(36, 9);
+            Console.WriteLine("----------");
+            //-------------------------------
+
+            //nom joueur 2
+
+            Console.SetCursorPosition(68, 1);
+            Console.WriteLine("nom joueur2");
+
+            //montent joueur 1
+
+            Console.SetCursorPosition(84, 1);
+            Console.WriteLine("montent joueur2");
+
+            //premier carte joueur 1
+
+            Console.SetCursorPosition(68, 3);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(68, 4 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(68, 9);
+            Console.WriteLine("----------");
+
+            //deuxiem carete joueur 1
+
+            Console.SetCursorPosition(84, 3);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(84, 4 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(84, 9);
+            Console.WriteLine("----------");
+            //------------------------------------
+
+            //nom joueur 3
+
+            Console.SetCursorPosition(20, 38);
+            Console.WriteLine("montent jour11");
+
+            //montent joueur 3
+
+            Console.SetCursorPosition(36, 38);
+            Console.WriteLine("nom joueur");
+
+            //premier carte joueur 3
+
+            Console.SetCursorPosition(20, 30);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(20, 31 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(20, 36);
+            Console.WriteLine("----------");
+
+            //deuxiem carete joueur 3
+
+            Console.SetCursorPosition(36, 30);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(36, 31 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(36, 36);
+            Console.WriteLine("----------");
+            //-------------------------------
+
+            //nom joueur 4
+
+            Console.SetCursorPosition(68, 38);
+            Console.WriteLine("nom joueur2");
+
+            //montent joueur 4
+
+            Console.SetCursorPosition(84, 38);
+            Console.WriteLine("montent joueur2");
+
+            //premier carte joueur 4
+
+            Console.SetCursorPosition(68, 30);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(68, 31 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(68, 36);
+            Console.WriteLine("----------");
+
+            //deuxiem carete joueur 4
+
+            Console.SetCursorPosition(84, 30);
+            Console.WriteLine("----------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(84, 31 + i);
+                Console.WriteLine("|        |");
+            }
+            Console.SetCursorPosition(84, 36);
+            Console.WriteLine("----------");
+            //------------------------------------
+
+            //dealer
+            Console.SetCursorPosition(1, 15);
+            Console.WriteLine("BOB le dealer");
+            Console.SetCursorPosition(5, 16);
+            Console.WriteLine(" --- ");
+            Console.SetCursorPosition(6, 17);
+            Console.WriteLine("UwU");
+            Console.SetCursorPosition(5, 17);
+            Console.WriteLine("|");
+            Console.SetCursorPosition(9, 17);
+            Console.WriteLine("|");
+            Console.SetCursorPosition(5, 18);
+            Console.WriteLine(" --- ");
+            Console.SetCursorPosition(7, 19);
+            Console.WriteLine("|");
+            Console.SetCursorPosition(7, 20);
+            Console.WriteLine("|");
+            Console.SetCursorPosition(8, 20);
+            Console.WriteLine("/");
+            Console.SetCursorPosition(9, 19);
+            Console.WriteLine("/");
+            Console.SetCursorPosition(10, 18);
+            Console.WriteLine("/");
+            Console.SetCursorPosition(7, 21);
+            Console.WriteLine("|");
+            Console.SetCursorPosition(6, 22);
+            Console.WriteLine("/");
+            Console.SetCursorPosition(8, 22);
+            Console.WriteLine("|");
+            Console.SetCursorPosition(5, 23);
+            Console.WriteLine("/");
+            Console.SetCursorPosition(8, 23);
+            Console.WriteLine("|");
         }
         public void UpdateGagnant(Joueur j)
         {
